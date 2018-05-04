c37a016e4036df56d723820d910ce469d4cdff16
Kavindra <sanjeevaniekv@gmail.com>
2018-05-04 23:57:26 +1000

    Updated to send requests using http instead of using client libraries.

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,28 @@
-var Mailgun = require('mailgun-js');
-var sendgrid = require('@sendgrid/mail');
+const Mailgun = require('mailgun-js');
+const sendgrid = require('@sendgrid/mail');
+const http = require('https');
+const querystring = require('querystring');
+const config = require('../config');
 
 module.exports = function(app) {
 
     // Mailgun API key
-    var api_key_mailgun = 'MAILGUN_API_KEY';
+    const api_key_mailgun = config.api_key_mailgun;
 
     // Default From email address
-    var from_mail = 'DEFAULT_SENDER_EMAIL';
+    const from_mail = config.from_mail;
 
-    // Set Sendgrid API key
-    sendgrid.setApiKey('SENDGRID_API_KEY');
+    // Sendgrid API key
+    const api_key_sendgrid = config.api_key_sendgrid;
 
     // Domain name for Mailgun
-    var domain = 'DOMAIN';
+    const domain = config.domain;
+
+    // Get base URL for mailgun from config file
+    const mail_gun_url = config.mail_gun_url;
+
+    // Get base URL for sendgrid from config file
+    const send_grid_url = config.send_grid_url;
 
     /*
     * Handle POST '/send' requests from the client.
@@ -38,6 +47,7 @@ module.exports = function(app) {
         sendWithMailgun(data, function(result) {
             console.log('Successfully sent email!!!');
             // Send success message to the client.
+            console.log(result);
             res.status(200).json({email: req.body.tomail});
         }, function (err) {
             console.log('Error occurred ', err);
@@ -64,16 +74,45 @@ module.exports = function(app) {
     * Parameters are data(message) object and callback function.
     * */
     function sendWithMailgun(data, successcallback, errorcallback) {
-        var mailgun = new Mailgun({apiKey: api_key_mailgun, domain: domain});
+        // Data to pass as the request body.
+        const postData = querystring.stringify(data);
 
-        // Send email with Mailgun's messages.send() function
-        mailgun.messages().send(data, function (err, body) {
-            if (err) {
-                errorcallback(err);
-            } else {
-                successcallback();
-            }
+        // HTTP options
+        const options = {
+            hostname: mail_gun_url,
+            path: '/v3/' + domain + '/messages',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(postData)
+            },
+            auth: 'api:' + api_key_mailgun
+        };
+
+        // HTTP request
+        const req = http.request(options, (res) => {
+            console.log(`STATUS: ${res.statusCode}`);
+            res.setEncoding('utf8');
+            let body = '';
+            // If the response is large, it receives as multiple chunks.
+            res.on('data', (chunk) => {
+                body += chunk;
+            });
+            // After receiving all the chunks, invoke success call back or error callback according to the status code.
+            res.on('end', () => {
+                res.statusCode === 200 ? successcallback(body) : errorcallback(body);
+            });
         });
+
+        // Handle request errors.
+        req.on('error', (e) => {
+            console.error(`problem with request: ${e.message}`);
+            errorcallback(e);
+        });
+
+        // write data to request body
+        req.write(postData);
+        req.end(); // End request
     }
 
     /*
@@ -81,14 +120,83 @@ module.exports = function(app) {
     * Parameters are data(message) object and callback function.
     * */
     function sendWithSendgrid(data, successcallback, errorcallback) {
-        // Send email with Sendgrid's send() function
-        sendgrid.send(data, (error, result) => {
-            if (error) {
-                errorcallback(error);
-            } else {
-                successcallback();
+        // Data to send as request body.
+        let postData = {
+            personalizations: [
+                {
+                    to: [
+                        {
+                            email: data.to
+                        }
+                    ],
+                    subject: data.subject
+                }
+            ],
+            from: {
+                email: data.from
+            },
+            content: [{
+                type: 'text/plain',
+                value: data.html
+            }]
+        };
+        // Assign CC email addresses if present
+        if(data.cc) {
+            let splitCc = data.cc.split(',');
+            let cc = splitCc.map(obj => {
+                var rObj = {};
+                rObj['email'] = obj.trim();
+                return rObj;
+            });
+            postData['personalizations'][0]['cc'] = cc;
+        }
+        // Assign BCC email addresses if present
+        if(data.bcc) {
+            let splitCc = data.bcc.split(',');
+            let bcc = splitCc.map(obj => {
+                var rObj = {};
+                rObj['email'] = obj;
+                return rObj;
+            });
+            postData['personalizations'][0]['bcc'] = bcc;
+        }
+
+        // HTTP options
+        const options = {
+            hostname: send_grid_url,
+            path: '/v3/mail/send',
+            port: null,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + api_key_sendgrid
             }
+        };
+
+        // Send HTTP request
+        const req = http.request(options, (res) => {
+            console.log(`STATUS: ${res.statusCode}`);
+            res.setEncoding('utf8');
+            let body = '';
+            res.on('data', (chunk) => {
+                body += chunk;
+            });
+
+            // After receiving all the chunks, invoke success call back or error callback according to the status code.
+            res.on('end', () => {
+                res.statusCode <= 202 ? successcallback(body) : errorcallback(body);
+            });
         });
+
+        // Handle request errors.
+        req.on('error', (e) => {
+            console.error(`problem with request: ${e.message}`);
+            errorcallback(e);
+        });
+
+        // write data to request body
+        req.write(JSON.stringify(postData));
+        req.end(); // End request
     }
 
 };
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "api_key_mailgun": "key-a0e1a10dcf3df1792f42b036d607d1d3",
+    "from_mail": "sanjeevaniekv@gmail.com",
+    "api_key_sendgrid": "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",
+    "domain": "sandboxf12129bdd3df40d6a824b0840560eade.mailgun.org",
+    "mail_gun_url": "api.mailgun.net",
+    "send_grid_url": "api.sendgrid.com"
+}
