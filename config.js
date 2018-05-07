be8bfe646b034c40428b6cab8c8c7daf46595599
Kavindra <sanjeevaniekv@gmail.com>
2018-05-07 20:08:06 +1000

    Implemented to send requests with HTTP client instead of using client libraries.

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    "api_key_mailgun": "key-a0e1a10dcf3df1792f42b036d607d1d3",
-    "from_mail": "sanjeevaniekv@gmail.com",
-    "api_key_sendgrid": "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",
-    "domain": "sandboxf12129bdd3df40d6a824b0840560eade.mailgun.org",
+    "api_key_mailgun": "YOUR_MAILGUN_KEY",
+    "from_mail": "DEFAULT_FROM_EMAIL",
+    "api_key_sendgrid": "YOUR_SENDGRID_KEY",
+    "domain": "DOMAIN_FOR_MAILGUN",
     "mail_gun_url": "api.mailgun.net",
     "send_grid_url": "api.sendgrid.com"
 }
